23cc0deaaa19b36d1dcec53d89cda5eeac049db5
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-02-22 18:14:32 +0500

    Add files via upload

---
+++ proglab5/Main.java
@@ -0,0 +1,30 @@
+package proglab5;
+import java.util.Scanner;
+
+import proglab5.commands.*;
+import proglab5.managers.CollectionManager;
+import proglab5.managers.CommandInvoker;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        CommandInvoker commandinvoker = new CommandInvoker();
+        CollectionManager collectionmanager = new CollectionManager();
+
+        commandinvoker.register(new Help());
+        commandinvoker.register(new Info(collectionmanager));
+        commandinvoker.register(new Show(collectionmanager));
+        commandinvoker.register(new Clear(collectionmanager));
+        commandinvoker.register(new Exit());
+        commandinvoker.register(new Add(scanner, collectionmanager));
+        commandinvoker.register(new RemoveById(collectionmanager));
+        commandinvoker.register(new History(commandinvoker));
+
+        while(true){
+            String commandName = scanner.nextLine();
+
+            commandinvoker.execute(commandName);
+        }
+
+    }
+}
