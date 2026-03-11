fcc44f9273ff0992c3984c451acbda328654a851
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:44:58 +0500

    Refactor AddIfMax to Clear command functionality

--- proglab5/commands/AddIfMax.java
+++ proglab5/commands/AddIfMax.java
@@ -1,29 +1,21 @@
 package proglab5.commands;
 
-import java.util.Collections;
-import java.util.Scanner;
-
 import proglab5.managers.CollectionManager;
-import proglab5.models.*;
 
 /**
- * Команда 'add_if_max'. Добавляет новый элемент в коллекцию, 
- * если его значение превышает значение наибольшего элемента коллекции.
+ * Команда 'clear'. Очищает коллекцию
  * @author XomyakFake
  */
+public class Clear implements Command {
+    private final CollectionManager collectionmanager;
 
-public class AddIfMax implements Command {
-    private final CollectionManager cm;
-    private final Scanner scanner;
-
-    public AddIfMax(Scanner scanner, CollectionManager cm){
-        this.scanner = scanner;
-        this.cm = cm;
+    public Clear(CollectionManager collectionmanager){
+        this.collectionmanager = collectionmanager;
     }
 
     @Override 
     public String getName(){
-        return "add_if_max";
+        return "clear";
     }
 
     /**
@@ -31,7 +23,7 @@ public class AddIfMax implements Command {
      */
     @Override
     public String getDescription(){
-        return "{element} добавить новый элемент в коллекцию, если его значение превышает значение наибольшего элемента этой коллекции";
+        return "очистить коллекцию";
     }
 
     /**
@@ -40,36 +32,8 @@ public class AddIfMax implements Command {
      */
     @Override
     public void execute(String... args){
-        Movie movie;
-        if(args.length > 0 && !args[0].isEmpty()){
-            movie = Movie.fromArrayNoId(args[0].split(",",-1));
-            if(movie == null){
-                System.out.println("Ошибка парсинга");
-                return;
-            }
-        }
-        else if(ExecuteScript.isExecuting){
-            System.out.println("В строке не укзаны аргументы для команды");
-            return;
-        }
-        else{
-            movie = Ask.askMovie(scanner, cm);
-        }
-        movie.setId(cm.generateId());
-        if(!movie.validate()){
-            System.out.println("Некоректный ввод фильма");
-        }
-        else if(cm.getCollection().isEmpty()){
-            cm.addMovie(movie);
-            System.out.println("Фильм добавлен");
-        }
-        else if((Collections.max(cm.getCollection())).compareTo(movie) < 0){
-            cm.addMovie(movie);
-            System.out.println("Фильм добавлен");
-        }
-        else{
-            System.out.println("Фильм меньше максимального в коллекции");
-        }
+        collectionmanager.clearCollection();
+        System.out.println("Коллекция очищена");
     }
     
 }
