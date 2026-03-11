8b95ba79c5807c3f701d8395d19dafaac2b5c98e
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:46:44 +0500

    Update Command.java

--- proglab5/commands/Command.java
+++ proglab5/commands/Command.java
@@ -1,6 +1,21 @@
 package proglab5.commands;
 
 public interface Command {
+
+    /**
+     * Исполнение команды
+     * @param args Аргументы необходимые команде.
+     * @return Успешность выполнения программы
+     */
     void execute(String ... args);
+
+    /**
+     * @return Название команды
+     */
     String getName();
+
+    /**
+     * @return Описание команды.
+     */
+    String getDescription();
 }
