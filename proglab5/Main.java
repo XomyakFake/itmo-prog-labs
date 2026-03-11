5ec370604f47fa9bc13ce9a090c75ac960445b64
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:58:22 +0500

    Integrate DumpManager and improve error handling
    Refactor Main class to include DumpManager and handle exceptions.

--- proglab5/Main.java
+++ proglab5/Main.java
@@ -1,17 +1,21 @@
 package proglab5;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import proglab5.commands.*;
 import proglab5.managers.CollectionManager;
 import proglab5.managers.CommandInvoker;
+import proglab5.managers.DumpManager;
 
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         CommandInvoker commandinvoker = new CommandInvoker();
-        CollectionManager collectionmanager = new CollectionManager();
+        DumpManager dumpManager = new DumpManager();
+        CollectionManager collectionmanager = new CollectionManager(dumpManager);
+        collectionmanager.loadCollection();
 
-        commandinvoker.register(new Help());
+        commandinvoker.register(new Help(commandinvoker));
         commandinvoker.register(new Info(collectionmanager));
         commandinvoker.register(new Show(collectionmanager));
         commandinvoker.register(new Clear(collectionmanager));
@@ -25,13 +29,26 @@ public class Main {
         commandinvoker.register(new AddIfMax(scanner, collectionmanager));
         commandinvoker.register(new RemoveGreater(scanner, collectionmanager));
         commandinvoker.register(new Update(scanner, collectionmanager));
+        commandinvoker.register(new Save(collectionmanager));
+        commandinvoker.register(new ExecuteScript(commandinvoker));
 
         while(true){
-            if(!scanner.hasNextLine()){
+            try{
+                if(!scanner.hasNextLine()){
+                    break;
+                }
+                String commandName = scanner.nextLine();
+                commandinvoker.execute(commandName);
+            }
+            catch(NoSuchElementException e){
+                System.out.println("\nВвод прерван. Завершение работы");
                 break;
             }
-            String commandName = scanner.nextLine();
-            commandinvoker.execute(commandName);
+            catch(Exception e){
+                System.out.println("Ошибка: " + e.getMessage());
+            }
         }
     }
 }
+
+
