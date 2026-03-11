a1038466ab766cbaabdb40f3275c13bf8ad8f2cc
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:55:05 +0500

    Refactor Update command execution logic
    Updated the 'execute' method to handle input parsing and validation more robustly.

--- proglab5/commands/Update.java
+++ proglab5/commands/Update.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 import proglab5.managers.CollectionManager;
 import proglab5.models.*;
 
+/**
+ * Команда 'update'. Обновляет элемент коллекции по его id
+ * @author XomyakFake
+ */
 public class Update implements Command {
     private final CollectionManager cm;
     private final Scanner scanner;
@@ -19,28 +23,61 @@ public class Update implements Command {
         return "update";
     }
 
+    /**
+     * Описание команды
+     */
+    @Override
+    public String getDescription(){
+        return "{element} обновить значение элемента коллекции, id которого равен заданному";
+    }
+
+    /**
+     * Выполняет команду
+     * @return Успешность выполнения команды.
+     */
     @Override
     public void execute(String... args){
         if(args[0].isEmpty()){
-        System.out.println("Не введен id");
-        return;
+            System.out.println("Не введен id");
+            return;
         }
         try{
-            int id = Integer.parseInt(args[0]);
+            String[] parts = args[0].strip().split(" ", 2);
+            int id = Integer.parseInt(parts[0]);
             Movie del = null;
             for(Movie movie : cm.getCollection()){
-                if(movie.getId().equals(id)){
+                if(movie.getId() == id){
                     del = movie;
                     break;
                 }
             }
             if(del != null){
-                Movie new_movie = Ask.askMovie(scanner, cm);
-                new_movie.setId(id);
-                cm.getCollection().remove(del);
-                cm.addMovie(new_movie);
+                Movie movie;
+                if(parts.length > 1 && !parts[1].isEmpty()){
+                    movie = Movie.fromArrayNoId(parts[1].split(",",-1));
+                    if(movie == null){
+                        System.out.println("Ошибка парсинга");
+                        return;
+                    }
+                }
+                else if(ExecuteScript.isExecuting){
+                    System.out.println("В строке не укзаны аргументы для команды");
+                    return;
+                }
+                else{
+                    movie = Ask.askMovie(scanner, cm);
+                }
+                movie.setId(id);
+                
+                if(movie.validate()){
+                    cm.getCollection().remove(del);
+                    cm.addMovie(movie);
+                    System.out.println("Данные обновлены");
+                }
+                else{
+                    System.out.println("Неккоректные данные. Обновление отменено");
+                }
 
-                System.out.println("Данные обновлены");
             }
             else{
                 System.out.println("Такого id не существует");
