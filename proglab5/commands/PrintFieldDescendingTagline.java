3fd8369e658ae547e539ee00853773d99ff4bd16
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:53:36 +0500

    Implement print_field_descending_tagline command
    Added command to print tagline values in descending order.

--- proglab5/commands/PrintFieldDescendingTagline.java
+++ proglab5/commands/PrintFieldDescendingTagline.java
@@ -8,6 +8,10 @@ import proglab5.managers.CollectionManager;
 import proglab5.models.Movie;
 import java.util.Comparator;
 
+/**
+ * Команда 'print_field_descending_tagline'. Выводит значения поля tagline всех элементов в порядке убывания
+ * @author XomyakFake
+ */
 public class PrintFieldDescendingTagline implements Command {
     private final CollectionManager cm;
 
@@ -20,6 +24,18 @@ public class PrintFieldDescendingTagline implements Command {
         return "print_field_descending_tagline";
     }
 
+    /**
+     * Описание команды
+     */
+    @Override
+    public String getDescription(){
+        return "вывести значения поля tagline всех элементов в порядке убывания";
+    }
+
+    /**
+     * Выполняет команду
+     * @return Успешность выполнения команды.
+     */
     @Override
     public void execute(String... args) {  
         if(cm.getCollection().isEmpty()){
@@ -40,8 +56,6 @@ public class PrintFieldDescendingTagline implements Command {
                 return tag2.compareTo(tag1);
             }
         });
-            
-        taglines.sort(Collections.reverseOrder());
         for(Movie movie : taglines){
             System.out.println("id=" + movie.getId() + ": " + movie.getTagline());
         }
