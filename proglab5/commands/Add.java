cdb57a79515079946778310f2ae5def068ff6df4
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:44:30 +0500

    Enhance Add command to accept movie details as arguments
    Updated the Add command to handle input arguments for adding a movie.

--- proglab5/commands/Add.java
+++ proglab5/commands/Add.java
@@ -1,10 +1,13 @@
 package proglab5.commands;
 
 import java.util.Scanner;
-
 import proglab5.managers.CollectionManager;
 import proglab5.models.*;
 
+/**
+ * Команда 'add'. Добавляет новый элемент в коллекцию
+ * @author XomyakFake
+ */
 public class Add implements Command {
     private final CollectionManager cm;
     private final Scanner scanner;
@@ -19,9 +22,35 @@ public class Add implements Command {
         return "add";
     }
 
+    /**
+     * Описание команды
+     */
+    @Override
+    public String getDescription(){
+        return "{element} добавить новый элемент в коллекцию";
+    }
+    
+    /**
+     * Выполняет команду
+     * @return Успешность выполнения команды.
+     */
     @Override
     public void execute(String... args){
-        Movie movie = Ask.askMovie(scanner, cm);
+        Movie movie;
+        if(args.length > 0 && !args[0].isEmpty()){
+            movie = Movie.fromArrayNoId(args[0].split(",",-1));
+            if(movie == null){
+                System.out.println("Ошибка парсинга");
+                return;
+            }
+        }
+        else if(ExecuteScript.isExecuting){
+            System.out.println("В строке не укзаны аргументы для команды");
+            return;
+        }
+        else{
+            movie = Ask.askMovie(scanner, cm);
+        }
         movie.setId(cm.generateId());
         if(movie.validate()){
             cm.addMovie(movie);
