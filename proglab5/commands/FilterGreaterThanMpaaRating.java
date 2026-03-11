6fced8aaaca33cd0166097998f927252cbefb8c0
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:47:53 +0500

    Implement filter_greater_than_mpaa_rating command
    Added command to filter movies by MPAA rating.

--- proglab5/commands/FilterGreaterThanMpaaRating.java
+++ proglab5/commands/FilterGreaterThanMpaaRating.java
@@ -4,6 +4,11 @@ import proglab5.managers.CollectionManager;
 import proglab5.models.Movie;
 import proglab5.models.MpaaRating;
 
+/**
+ * Команда 'filter_greater_than_mpaa_rating'. Выводит элементы 
+ * значения поля поля mpaaRating которых больше заданного
+ * @author XomyakFake
+ */
 public class FilterGreaterThanMpaaRating implements Command {
     private final CollectionManager cm;
 
@@ -15,7 +20,19 @@ public class FilterGreaterThanMpaaRating implements Command {
     public String getName(){
         return "filter_greater_than_mpaa_rating";
     }
+    
+    /**
+     * Описание команды
+     */
+    @Override
+    public String getDescription(){
+        return "{mpaaRating} вывести элементы, значение поля mpaaRating которых больше заданного";
+    }
 
+    /**
+     * Выполняет команду
+     * @return Успешность выполнения команды.
+     */
     @Override
     public void execute(String... args) {  
         if(args[0].isEmpty()){
