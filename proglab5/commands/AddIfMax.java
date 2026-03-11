6bf9a5a8f6852647e6203a445890023dc30c5618
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:44:46 +0500

    Implement 'add_if_max' command functionality
    Add command to insert element if it is greater than the max element in the collection.

--- proglab5/commands/AddIfMax.java
+++ proglab5/commands/AddIfMax.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 import proglab5.managers.CollectionManager;
 import proglab5.models.*;
 
+/**
+ * Команда 'add_if_max'. Добавляет новый элемент в коллекцию, 
+ * если его значение превышает значение наибольшего элемента коллекции.
+ * @author XomyakFake
+ */
+
 public class AddIfMax implements Command {
     private final CollectionManager cm;
     private final Scanner scanner;
@@ -20,9 +26,35 @@ public class AddIfMax implements Command {
         return "add_if_max";
     }
 
+    /**
+     * Описание команды
+     */
+    @Override
+    public String getDescription(){
+        return "{element} добавить новый элемент в коллекцию, если его значение превышает значение наибольшего элемента этой коллекции";
+    }
+
+    /**
+     * Выполняет команду
+     * @return Успешность выполнения команды.
+     */
     @Override
     public void execute(String... args){
-        Movie movie = Ask.askMovie(scanner, cm);
+        Movie movie;
+        if(args.length > 0 && !args[0].isEmpty()){
+            movie = Movie.fromArrayNoId(args[0].split(",",-1));
+            if(movie == null){
+                System.out.println("Ошибка парсинга");
+                return;
+            }
+        }
+        else if(ExecuteScript.isExecuting){
+            System.out.println("В строке не укзаны аргументы для команды");
+            return;
+        }
+        else{
+            movie = Ask.askMovie(scanner, cm);
+        }
         movie.setId(cm.generateId());
         if(!movie.validate()){
             System.out.println("Некоректный ввод фильма");
