afc87c9f744dcfe755043bccf058fb0443b22913
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2026-03-11 17:54:12 +0500

    Enhance RemoveGreater command functionality
    Added command description and improved argument handling.

--- proglab5/commands/RemoveGreater.java
+++ proglab5/commands/RemoveGreater.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 import proglab5.managers.CollectionManager;
 import proglab5.models.*;
 
+/**
+ * Команда 'remove_greater'. Удаляет элементы из коллекции превышающие заданный
+ * @author XomyakFake
+ */
 public class RemoveGreater implements Command {
     private final CollectionManager cm;
     private final Scanner scanner;
@@ -20,9 +24,35 @@ public class RemoveGreater implements Command {
         return "remove_greater";
     }
 
+    /**
+     * Описание команды
+     */
+    @Override
+    public String getDescription(){
+        return "{element} удалить из коллекции все элементы, превышающие заданный";
+    }
+
+    /**
+     * Выполняет команду
+     * @return Успешность выполнения команды.
+     */
     @Override
     public void execute(String... args){
-        Movie movie = Ask.askMovie(scanner, cm);
+        Movie movie;
+        if(args.length > 0 && !args[0].isEmpty()){
+            movie = Movie.fromArrayNoId(args[0].split(",",-1));
+            if(movie == null){
+                System.out.println("Ошибка парсинга");
+                return;
+            }
+        }
+        else if(ExecuteScript.isExecuting){
+            System.out.println("В строке не укзаны аргументы для команды");
+            return;
+        }
+        else{
+            movie = Ask.askMovie(scanner, cm);
+        }
         int curr_size = cm.getCollection().size();
         Iterator<Movie> iterator = cm.getCollection().iterator();
         while(iterator.hasNext()){
